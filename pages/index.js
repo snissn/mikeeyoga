a45da5b0d8c09c3b9892631ab96b352c23861388
mikers <snissn@gmail.com>
2020-08-29 02:40:50 -0400

    check in youtube code

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,35 @@ var parseString = require('xml2js').parseString;
 
 import "isomorphic-fetch"
 
+async function xml2json(xml) {
+  return new Promise((resolve, reject) => {
+      parseString(xml, { explicitArray : false, ignoreAttrs : true }, function (err, json) {
+          if (err)
+              reject(err);
+          else
+              resolve(json);
+      });
+
+  });
+}
+
+
+
 let youtube = fetch("https://thingproxy.freeboard.io/fetch/https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg").then(res=>{
   res.text().then((xml,b)=>{
     parseString(xml, { explicitArray : false, ignoreAttrs : true },function (err, result) {
       const feed = result['feed']['entry']
-      console.dir((feed));
+      //console.dir((feed));
   });
     
   });
  })
 
-export default () => (
+ function Index({feed}){
+    var video = feed[0];
+   
+   const videoUrl = 'https://www.youtube-nocookie.com/embed/'+video['yt:videoId']
+ return (
   <Layout>
     <section className="section">
       <div className="container has-text-centered">
@@ -25,14 +43,10 @@ export default () => (
 
         <p className="video" >
         
-        <iframe width="500" height="900" src="https://www.youtube-nocookie.com/embed/oU0tPLvUg-o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="500" height="900" src={videoUrl} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </p>
 
-
-
-
-
-        
+         
       </div>
     </section>
 
@@ -41,7 +55,9 @@ export default () => (
         
         section {
           background: #24589f;
+          
         }
+        .card{width:400px;}
         h1{
           color: #f8f8ff;
 
@@ -55,3 +71,20 @@ export default () => (
 
   </Layout>
 );
+
+      }
+
+      Index.getInitialProps = async ctx => {
+        let youtube = await fetch("https://thingproxy.freeboard.io/fetch/https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg")
+        
+        let xml = await youtube.text();
+        var x = await xml2json(xml);
+         parseString(xml, { explicitArray : false, ignoreAttrs : true },function (err, result) {
+          const feed = result['feed']['entry']
+          //console.dir((feed));
+      });
+      const feed = x['feed']['entry']
+        
+        return {feed:feed}
+      }
+      export default Index;
