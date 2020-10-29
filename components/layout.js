eddab2a39bd0f6ec1122c13f5569678e64107329
Mike Seiler <snissn@gmail.com>
2020-10-29 10:41:33 -0400

    add logo and prettyify

--- components/layout.js
+++ components/layout.js
@@ -1,80 +1,83 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import '../styles/styles.sass'
+import Head from "next/head";
+import Link from "next/link";
+import "../styles/styles.sass";
 
 export default ({ children }) => {
   /*
    * Added this to toggle the is-active class. See:
-   * 
+   *
    * https://bulma.io/documentation/components/navbar/#navbar-menu
    * https://github.com/jgthms/bulma/issues/856
    */
   const toggleStyles = (event) => {
-    document.querySelector('#burger').classList.toggle('is-active')
-    document.querySelector('#navbarmenu').classList.toggle('is-active')
-  }
+    document.querySelector("#burger").classList.toggle("is-active");
+    document.querySelector("#navbarmenu").classList.toggle("is-active");
+  };
 
   return (
     <div>
       <Head>
         <title>Mikee Yoga</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          />
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        />
       </Head>
       <header>
         <nav className="navbar " role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
             <a className="navbar-item">
-              Mikee Yoga
+              <img src="/logo.png" style={{ height: "120px" }} />
             </a>
-        
-              <Link prefetch href="/">
-                <a className="navbar-item">Home</a>
-              </Link>
-              <Link prefetch href="/youtube">
-                <a className="navbar-item">Youtube Classes</a>
-              </Link>
-              <Link prefetch href="/about">
-                <a className="navbar-item">About</a>
-              </Link>
-            </div>
-       
 
+            <Link prefetch href="/">
+              <a className="navbar-item">Home</a>
+            </Link>
+            <Link prefetch href="/youtube">
+              <a className="navbar-item">Youtube Classes</a>
+            </Link>
+            <Link prefetch href="/about">
+              <a className="navbar-item">About</a>
+            </Link>
+          </div>
         </nav>
       </header>
       {children}
       <footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>Mikee</strong> is a Yoga teacher based in Brooklyn, NY.</p>
-    <p>
-    <button
-              className="button   is-link "
-               
-            >
-    <span className="icon">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Mikee</strong> is a Yoga teacher based in Brooklyn, NY.
+          </p>
+          <p>
+            <button className="button   is-link ">
+              <span className="icon">
                 <i className="fab fa-instagram" />
-              </span> <a href="https://instagram.com/MikeeYoga" style={{color:'white'}}>Follow @MikeeYoga on Instagram</a>
-              </button>
-              <br />
-              <br />
-              <button
-              className="button   is-link "
-               
-            >
-    <span className="icon">
+              </span>{" "}
+              <a
+                href="https://instagram.com/MikeeYoga"
+                style={{ color: "white" }}
+              >
+                Follow @MikeeYoga on Instagram
+              </a>
+            </button>
+            <br />
+            <br />
+            <button className="button   is-link ">
+              <span className="icon">
                 <i className="fab fa-youtube" />
-              </span> <a href="              https://www.youtube.com/channel/UC5wC5_0J9Qa6MxXoI8DPUAg
-" style={{color:'white'}}>Follow @MikeeYoga on Youtube</a>
-              </button>
-
-    </p>
-  </div>
-</footer>
-
+              </span>{" "}
+              <a
+                href="              https://www.youtube.com/channel/UC5wC5_0J9Qa6MxXoI8DPUAg
+"
+                style={{ color: "white" }}
+              >
+                Follow @MikeeYoga on Youtube
+              </a>
+            </button>
+          </p>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,122 @@
-import Layout from '../components/layout'
-var parseString = require('xml2js').parseString;
+import Layout from "../components/layout";
+var parseString = require("xml2js").parseString;
 
-import useSWR from 'swr'
+import useSWR from "swr";
 
-import "isomorphic-fetch"
+import "isomorphic-fetch";
 
 async function xml2json(xml) {
   return new Promise((resolve, reject) => {
-      parseString(xml, { explicitArray : false, ignoreAttrs : true }, function (err, json) {
-          if (err)
-              reject(err);
-          else
-              resolve(json);
-      });
-
+    parseString(xml, { explicitArray: false, ignoreAttrs: true }, function(
+      err,
+      json
+    ) {
+      if (err) reject(err);
+      else resolve(json);
+    });
   });
 }
-async function swfFetchYoutube(url){
-  let youtube = await fetch(url)
+async function swfFetchYoutube(url) {
+  let youtube = await fetch(url);
   let xml = await youtube.text();
   var x = await xml2json(xml);
-  const feed = x['feed']['entry']
+  const feed = x["feed"]["entry"];
   var video = feed[0];
-  var data = await getVideoProps(feed[0]['yt:videoId'])
-  video['data'] = data;
-  return video
+  var data = await getVideoProps(feed[0]["yt:videoId"]);
+  video["data"] = data;
+  return video;
 }
 
-async function getVideoProps(videoId){
-  const youtube = await fetch("https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v="+videoId+"&format=json")
-  var data = await youtube.json()
+async function getVideoProps(videoId) {
+  const youtube = await fetch(
+    "https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=" +
+      videoId +
+      "&format=json"
+  );
+  var data = await youtube.json();
   return data;
 }
 
- 
- function Index({video}){
-  const { data, error } = useSWR("https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg", swfFetchYoutube)
+function Index({ video }) {
+  const { data, error } = useSWR(
+    "https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg",
+    swfFetchYoutube
+  );
   //make sure data is updated each time
   let videoUrl;
-  if(data){    
-     videoUrl = 'https://www.youtube-nocookie.com/embed/'+data['yt:videoId'];
-  }else{
-     videoUrl = 'https://www.youtube-nocookie.com/embed/'+video['yt:videoId'];
+  if (data) {
+    videoUrl = "https://www.youtube-nocookie.com/embed/" + data["yt:videoId"];
+  } else {
+    videoUrl = "https://www.youtube-nocookie.com/embed/" + video["yt:videoId"];
   }
-   return (
-  <Layout>
-    <section className="section">
-      <div className="container has-text-centered">
-      <h1 className="title">Hello I'm Mikee</h1> 
+  return (
+    <Layout>
+      <section className="section">
+        <div className="container has-text-centered">
+          <p>
+            <img src="/Hello.png" />
+          </p>
+        </div>
+      </section>
+      <section className="section bottom">
+        <div className="container has-text-centered">
+          <p className="video">
+            <iframe
+              width={video.data.thumbnail_width}
+              style={{
+                height: video.data.thumbnail_height,
+                width: video.data.thumbnail_width,
+              }}
+              height={video.data.thumbnail_height}
+              src={videoUrl}
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </p>
+          <p>
+            <h1 className="title">My latest yoga video</h1>
+          </p>
+        </div>
+      </section>
 
-        <p><img  src="/Hello.png" /></p>
-
-
-
-
-         
-      </div>
-    </section>
-    <section className="section bottom">
-      <div className="container has-text-centered">
-    <p className="video" >
-        
-        <iframe width={video.data.thumbnail_width} style={{height:video.data.thumbnail_height,width:video.data.thumbnail_width}} height={video.data.thumbnail_height} src={videoUrl} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        
-        </p>
-        <p><h1 className="title">My latest yoga video</h1></p>
-        
-         
-      </div>
-    </section>
-
-
-    <style jsx>{`
-        
+      <style jsx>{`
         section {
           background: #24589f;
-          
         }
-        .section.bottom{
-          background:#1d2110;
+        .section.bottom {
+          background: #1d2110;
         }
-        .card{ }
-        h1{
+        .card {
+        }
+        h1 {
           color: #f8f8ff;
-
         }
-        .video, .video > iframe{
-          
+        .video,
+        .video > iframe {
         }
-  
       `}</style>
+    </Layout>
+  );
+}
 
+Index.getInitialProps = async (ctx) => {
+  let youtube;
 
-  </Layout>
-);
+  youtube = await fetch(
+    "https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg"
+  );
 
-      }
+  let xml = await youtube.text();
+  var x = await xml2json(xml);
 
-      Index.getInitialProps = async ctx => {
-        let youtube;
-        
-         youtube = await fetch("https://bold-meadow-4a42.mike4921.workers.dev/?https://www.youtube.com/feeds/videos.xml?channel_id=UC5wC5_0J9Qa6MxXoI8DPUAg")
-        
-        
-        let xml = await youtube.text();
-        var x = await xml2json(xml);
- 
-      const feed = x['feed']['entry']
-      var video = feed[0];
-   
-      var data = await getVideoProps(feed[0]['yt:videoId'])
-       video['data'] = data;
-      console.log(data);
-   
-        return {video}
-      }
-      export default Index;
+  const feed = x["feed"]["entry"];
+  var video = feed[0];
+
+  var data = await getVideoProps(feed[0]["yt:videoId"]);
+  video["data"] = data;
+  console.log(data);
+
+  return { video };
+};
+export default Index;
