be01ca770e995b3c29701af7c24712f36600cfb6
mikers <snissn@gmail.com>
2020-11-14 23:43:21 -0500

    autoplay

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,50 +1,40 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-import { Carousel } from 'react-responsive-carousel';
-
-
-
+import { Carousel } from "react-responsive-carousel";
 
 export default (props) => {
+  return (
+    <section className="section">
+      <Carousel autoPlay={true} style={{ maxWidth: 100 }}>
+        <div>
+          <img src="/1.jpg" />
+        </div>
 
-	return (
-		<section className="section" >
-			<Carousel style={{ maxWidth: 100 }}>
+        <div>
+          <img src="/2.jpg" />
+        </div>
 
+        <div>
+          <img src="/7.jpg" />
+        </div>
 
-				<div>
-					<img src="/1.jpg" />
-				</div>
+        <div>
+          <img src="/3.jpg" />
+        </div>
 
-				<div>
-					<img src="/2.jpg" />
-				</div>
+        <div>
+          <img src="/4.jpg" />
+        </div>
 
-				<div>
-					<img src="/7.jpg" />
-				</div>
+        <div>
+          <img src="/6.jpg" />
+        </div>
 
-
-				<div>
-					<img src="/3.jpg" />
-				</div>
-
-				<div>
-					<img src="/4.jpg" />
-				</div>
-
-				<div>
-					<img src="/6.jpg" />
-				</div>
-
-
-				<div>
-					<img src="/5.jpg" />
-				</div>
-
-			</Carousel>
-		</section>
-
-	)
-}
+        <div>
+          <img src="/5.jpg" />
+        </div>
+      </Carousel>
+    </section>
+  );
+};
